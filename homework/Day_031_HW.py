1115009809603b4189012ccf70d98321755501ab
Yucca03 <45850404+Yucca03@users.noreply.github.com>
2019-05-02 14:43:26 +0800

    Create Day_031_HW.py

---
+++ homework/Day_031_HW.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# ## 練習時間
+
+# 請觀看台大電機李宏毅教授的影片。李教授會用非常淺顯易懂的方式介紹甚麼是機器學習。
+# 
+
+
+from IPython.display import YouTubeVideo
+YouTubeVideo('CXgbekl66jc', width=720, height=480)
+
+
+# ## 請找出一個你認為機器學習可以發光發熱的領域並闡述原因? 
+
+# ### 可將機器學習用於醫藥界，以預測病患用藥組合，提前預防疾病的發生
