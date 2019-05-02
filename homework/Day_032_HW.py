c3957517dd6c876d6398614233bd7d81187430ef
Yucca03 <45850404+Yucca03@users.noreply.github.com>
2019-05-02 14:52:25 +0800

    Create Day_032_HW.py

---
+++ homework/Day_032_HW.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# ## 練習時間
+# 相信大家目前都能夠初步了解機器學習專案的流程及步驟，今天的作業希望大家能夠看看全球機器學習巨頭們在做的機器學習專案。以 google 為例，下圖是 Google 內部專案使用機器學習的數量，隨著時間進展，現在早已超過 2000 個專案在使用 ML。
+
+# ![image](https://cdn-images-1.medium.com/max/800/1*U_L8qI8RmYS-MOBrYvXhSA.png)
+
+# 底下幫同學整理幾間知名企業的 blog 或機器學習網站 (自行搜尋也可)，請挑選一篇文章閱讀並試著回答
+# 1. 專案的目標？ (要解決什麼問題）
+# 2. 使用的技術是？ (只需知道名稱即可，例如：使用 CNN 卷積神經網路做影像分類)
+# 3. 資料來源？ 
+
+# - [Google AI blog](https://ai.googleblog.com/)
+# - [Facebook Research blog](https://research.fb.com/blog/)
+# - [Apple machine learning journal](https://machinelearning.apple.com/)
+# - [機器之心](https://www.jiqizhixin.com/)
+# - [雷鋒網](http://www.leiphone.com/category/ai)
+
+# ### Optimizing Siri on HomePod in Far‑Field Settings
+# #### 專案的目標？ (要解決什麼問題）
+# ##### 1. remove echo and background noise
+# ##### 2. separate simultaneous sound sources and trigger-phrase based stream selection to eliminate interfering speech
+# 
+# #### 使用的技術是？ (只需知道名稱即可，例如：使用 CNN 卷積神經網路做影像分類)
+# ##### 1. DNN
+# 
+# #### 資料來源？
+# ##### apple HomePod 用戶
