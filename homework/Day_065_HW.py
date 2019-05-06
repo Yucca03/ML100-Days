0cab98c56bd4e8291b5547b451c6d96930621fbe
Yucca03 <45850404+Yucca03@users.noreply.github.com>
2019-05-06 16:32:56 +0800

    Create Day_065_HW.py

---
+++ homework/Day_065_HW.py
@@ -0,0 +1,6 @@
+# 作業
+# * 選擇分類資料集(右下)-螺旋雙臂 - 交錯六群，限定特徵只能使用前兩個，隱藏層 2 層滿 (共 8 * 2 =16 個神經元)，  
+#   遞迴次數只允許跑到500次，但可以自由調整 批次大小、學習速率、啟動函數、正規化選項與參數
+# * 在上述限制下，挑戰看看測試誤差 (Test Loss) 最低能到多少? 請回答你的上述幾項參數與 Test Loss 數
+# Test Loss: 0.483
+# Learning rate: 0.001, Activation: ReLU, Regularization: L2, Regularization rate: 0.03
